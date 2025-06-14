cbb9122945dd1b8523d9884d8979b9335e5563a7
DhananjayRathore <dhananjayrathore0307@gmail.com>
2025-06-14 13:14:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [name, setName] = useState('');
+  const [weight, setWeight] = useState('');
+  const [number, setNumber] = useState('');
+
+  const show = () => {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${number}`;
+    axios.get(URL)
+      .then((response) => {
+        setData(response.data);
+        setName(response.data.name);
+        setWeight(response.data.weight);
+      })
+      .catch((err) => {
+        alert("Pokémon not found");
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <div className='App1'>
+      <h1 className='title'>Pokemon Card Generator</h1>
+      <h3 className='id'>Enter ID between 1-999</h3>
+      <input
+        type="number"
+        onChange={(e) => setNumber(e.target.value)}
+        placeholder="Enter Pokémon ID "
+      />
+      <button onClick={show}>Show</button>
+
+      {data && (
+        <>
+          <h2 className='name'>Name: {name}</h2>
+          <h3 className='weight'>Weight: {weight}</h3>
+          <img className='img1'
+            src={data.sprites.other.dream_world.front_default}
+            alt={name}
+          />
+          <p className='abilities'>My abilities are:</p>
+          {data.abilities.map((value, key) => (
+            <div key={key}>{value.ability.name}</div>
+          ))}
+        </>
+      )}
+    </div>
     </div>
   );
 }
